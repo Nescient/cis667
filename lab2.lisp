98913e5a94a2dcf0369b0fc2883a57c5afc54f5b
sam <spam@yahoo.com>
2016-09-16 19:15:55 -0400

    added my-list function

--- lab2.lisp
+++ lab2.lisp
@@ -2,7 +2,14 @@
 ;; CIS667 Fall 2016
 ;; Lab 02 20160916
 
-(defun my-list ()
+(defun my-list (l1 &rest ls)
+   (labels ((my-list-recur (arg1 args)
+      (if args
+          (cons arg1 (my-list-recur (car args) (cdr args)))
+          arg1
+      )
+   ))
+   (my-list-recur l1 ls))
 )
 
 
