e2f80cdb745a9b369fa6bb05679ba838ffeb50bf
sam <spam@yahoo.com>
2016-10-01 22:17:47 -0400

    fixed the methods and heuristics so the arguments are correct now

--- lab4.lisp
+++ lab4.lisp
@@ -2,18 +2,14 @@
 ;; CIS667 Fall 2016
 ;; Lab 04 20161001
 
-(defun get-city-coords (city city-map)
-   (cadr (find-city city city-map))
-)
-
-(defun get-add-norm (city1 city2 city-map)
-   (x+y-distance (city-loc (find-city city1 city-map)) (city-loc (find-city city2 city-map)))
+(defun get-add-norm (city1 city2)
+   (x+y-distance (city-loc city1) (city-loc city2))
 )
 
 ;; based on x+y-distance from lisp utilities
-(defun get-max-norm (city1 city2 city-map)
-   (let ((p (city-loc (find-city city1 city-map)))
-         (q (city-loc (find-city city2 city-map)))
+(defun get-max-norm (city1 city2)
+   (let ((p (city-loc city1))
+         (q (city-loc city2))
         )
       (max (abs (- (xy-x p) (xy-x q)))
            (abs (- (xy-y p) (xy-y q)))
@@ -25,7 +21,6 @@
 (defmethod h1-cost ((problem route-finding-problem) city-name)
    (get-add-norm (find-city city-name problem)
                  (find-city (problem-goal problem) problem)
-                 problem
    )
 )
 
@@ -33,9 +28,8 @@
 (defmethod h2-cost ((problem route-finding-problem) city-name)
    (get-max-norm (find-city city-name problem)
                  (find-city (problem-goal problem) problem)
-                 problem
    )
 )
 
 ;; (SETQ SEARCHERS '(TREE-A*-SEARCH A*-SEARCH TREE-IDA*-SEARCH))
-;; ((compare-search-algorithms #'(lambda () (make-romanian-problem :goal 'Iasi :initial-state 'Timisoara)) SEARCHERS)
+;; (COMPARE-SEARCH-ALGORITHMS #'(LAMBDA NIL (MAKE-ROMANIAN-PROBLEM :initial-state 'Timisoara :GOAL 'IASI)) SEARCHERS)
