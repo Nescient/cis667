d296a7eb91e0141631488afc24cee67cf3edced1
sam <spam@yahoo.com>
2016-10-08 21:00:10 -0400

    added methods to decode and print

--- lab5.lisp
+++ lab5.lisp
@@ -2,3 +2,16 @@
 ;; CIS667 Fall 2016
 ;; Lab 05 20161008
 
+(defun decode (number)
+   (cond
+      ((<= number 0) nil)
+      (t (cons (mod number 9) (decode (/ (- number (mod number 9)) 9))))
+   )
+)
+
+(defun display (puzlist)
+   (print (subseq puzlist 0 3))
+   (print (subseq puzlist 3 6))
+   (print (subseq puzlist 6 9))
+   puzlist
+)
