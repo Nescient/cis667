6c5a497f21ee51021982d738e007f51ab1ee178f
sam <spam@yahoo.com>
2016-10-08 22:26:12 -0400

    added cases for lab 5

--- lab5.lisp
+++ lab5.lisp
@@ -12,7 +12,7 @@
 (defun custom-print (puzrow)
    (dolist (x puzrow)
       (cond
-         ((eq x nil) (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*))
+         ((equal x nil) (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*))
          ((eq x 0) (princ "."))
          (t (princ x))
       )
@@ -22,7 +22,14 @@
 
 (defun display (puzlist)
    (custom-print (subseq puzlist 0 3))
+   (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*)
    (custom-print (subseq puzlist 3 6))
+   (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*)
    (custom-print (subseq puzlist 6 9))
+   (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*)
    puzlist
+)
+
+(defun show-puz (number)
+   (display (decode number))
 )
