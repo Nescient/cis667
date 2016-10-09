af978f568c32af72fde07e98dc091e042b58157e
sam <spam@yahoo.com>
2016-10-08 22:43:05 -0400

    cleaned up method

--- lab5.lisp
+++ lab5.lisp
@@ -31,5 +31,63 @@
 )
 
 (defun show-puz (number)
-   (display (decode number))
+   (let ((l (decode number)))
+      (if (< (length l) 9) (setq l (nconc l (list 0))))
+      (display l)
+   )
+)
+
+(defvar *lab5case1* '(
+34160732
+120136076
+120346028
+86911172
+91162700
+93816260
+93775436
+88999028
+12471524
+184422212
+184776020
+184776236
+184769684
+175216868
+196474508
+200189492
+200195316
+200195308
+200182204
+199867276
+27916588
+219235348
+247893796
+))
+
+(defvar *lab5case2*
+'(274878620
+260549396
+277555508
+281270492
+281276316
+281276308
+281256652
+281151676
+281446516
+281446732
+281433628
+262327996
+32745484
+247683844
+247893796)
+)
+
+(defun lab5 ()
+   (dolist (x *lab5case1*)
+      (print x)
+      (princ ":")
+      (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*)
+      (show-puz x)
+      (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*)
+      (write-char #\return *standard-output*)(write-char #\linefeed *standard-output*)
+   )
 )
